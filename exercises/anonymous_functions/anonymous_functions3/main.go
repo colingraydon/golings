c88eb9bd5b1adf2bdadbcb53a73dd493e7dd934b
Colin Graydon <colingraydon@gmail.com>
2024-11-22 19:13:14 -0500

    feat: finish golings

--- exercises/anonymous_functions/anonymous_functions3/main.go
+++ exercises/anonymous_functions/anonymous_functions3/main.go
@@ -1,7 +1,6 @@
 // anonymous functions3
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
--- exercises/structs/structs2/main.go
+++ exercises/structs/structs2/main.go
@@ -1,15 +1,18 @@
 // structs2
 // Make me compile!
-//
-// I AM NOT DONE
 package main
 
 import "fmt"
 
+type Details struct {
+	phone string
+}
+
 type Person struct {
 	// don't just create the phone field here. embed a new struct
 	name string
-	age int
+	age  int
+	Details
 }
 
 func main() {
